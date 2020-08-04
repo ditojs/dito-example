3b98858d1d819aa73c9d00c798c330c1f806f2c4
Jürg Lehni <juerg@scratchdisk.com>
2020-08-04 15:02:47 +0200

    Use markup component in messages

--- src/admin/views/dummies/message.js
+++ src/admin/views/dummies/message.js
@@ -2,10 +2,23 @@ export const message = {
   compact: true,
   components: {
     text: {
+      type: 'markup',
+      label: false,
       width: '100%',
-      type: 'textarea',
-      label: 'Text',
-      required: true
+      lines: 4,
+      required: true,
+      resizable: true,
+      marks: {
+        bold: true,
+        italic: true,
+        strike: true,
+        underline: true,
+        link: true
+      },
+      // Replace non-breaking space with __ for better visibility while editing
+      format: ({ value }) => value?.replace(/(\xa0|&nbsp;)/g, '__') ?? value,
+      // ...and then back to a non-breaking space for storage:
+      parse: ({ value }) => value?.replace(/__/g, '\xa0') ?? value
     }
   }
 }
