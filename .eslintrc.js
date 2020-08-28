22edb0d346eb672c76ec0efeb1a28a680cabbcb3
Jürg Lehni <juerg@scratchdisk.com>
2020-08-28 17:13:28 +0200

    Update dependencies & eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,42 +3,57 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      legacyDecorators: true
+    }
   },
   env: {
     node: true
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: 'standard',
+  plugins: [
+    '@babel'
+  ],
   rules: {
+    'array-bracket-spacing': ['error', 'never'],
     // Allow paren-less arrow functions.
     'arrow-parens': ['error', 'as-needed'],
     // Allow async-await.
     'generator-star-spacing': 'off',
+    'indent': ['error', 2, {
+      flatTernaryExpressions: true
+    }],
     'max-len': ['error', 80, 2, {
       ignoreUrls: true,
       ignoreTemplateLiterals: true,
       ignoreRegExpLiterals: true
     }],
+    'no-cond-assign': 'error',
+    'no-const-assign': 'error',
+    'no-constant-condition': isProduction ? 'error' : 'warn',
     // Allow debugger during development.
     'no-debugger': isProduction ? 'error' : 'warn',
-    'no-constant-condition': isProduction ? 'error' : 'warn',
+    'no-mixed-operators': 'off',
+    'no-new': 'off',
+    'no-new-func': 'off',
+    'no-prototype-builtins': 'off',
+    'no-return-assign': 'error',
+    'no-this-before-super': 'error',
+    'no-undef': isProduction ? 'error' : 'warn',
+    'no-unreachable': isProduction ? 'error' : 'warn',
     'no-unused-vars': [isProduction ? 'error' : 'warn', {
       args: 'after-used',
       argsIgnorePattern: '^_',
       ignoreRestSiblings: true
     }],
-    'no-cond-assign': 'error',
-    'no-new': 'off',
-    'no-new-func': 'off',
-    'no-mixed-operators': 'off',
-    'no-return-assign': 'error',
     'no-var': 'error',
     'object-shorthand': 'error',
-    'standard/object-curly-even-spacing': ['off'],
+    'standard/object-curly-even-spacing': 'off',
     'object-curly-spacing': ['error', 'always'],
     'prefer-const': ['error', {
       destructuring: 'all'
@@ -49,15 +64,16 @@ module.exports = {
         array: false
       }
     }],
+    'quotes': ['error', 'single', {
+      allowTemplateLiterals: true,
+      avoidEscape: true
+    }],
     'quote-props': ['error', 'consistent-as-needed'],
     'space-before-function-paren': ['error', {
       anonymous: 'always',
       named: 'never',
       asyncArrow: 'always'
     }],
-    'space-in-parens': ['error', 'never'],
-    'indent': ['error', 2, {
-      flatTernaryExpressions: true
-    }]
+    'space-in-parens': ['error', 'never']
   }
 }
